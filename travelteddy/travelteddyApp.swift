99f0db5d60df11c8a4f5843f40ff193a8444a200
tara <goldensuperhero967@gmail.com>
2023-07-18 10:25:39 -0700

    Initial Commit

---
+++ travelteddy/travelteddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelteddyApp.swift
+//  travelteddy
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct travelteddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
